09ec47fa4a91e964c993e3d037027c736d9ce47a
LAB_J205 <labj205@continental.edu.pe>
2025-03-28 11:36:07 -0500

    Se codifico el codigo con el enfoque POO

--- Mayor-tres-numeros.py
+++ Mayor-tres-numeros.py
@@ -1,27 +1,48 @@
 '''
 Determina el mayor de 3 numeros ingresados por el teclado
 '''
-def intercambio_valores(numero1,numero2):
-    temporal = numero1
-    numero1 = numero2
-    numero2 = temporal
-    return numero1,numero2
+class ordenadoTresNumeros:
+    def __init__(self,numero1,numero2,numero3):
+        self.numero1 = numero1
+        self.numero2 = numero2
+        self.numero3 = numero3
 
-numero1=int(input("Ingresa el primer numero: "))
-numero2=int(input("Ingresa el segundo numero: "))
-numero3=int(input("Ingresa el tercer numero: "))
+    def intercambio_valores(self,numero1,numero2):
+        temporal = numero1
+        numero1 = numero2
+        numero2 = temporal
+        return numero1,numero2
+    def ingresar_numeros(self):
 
-if numero1>numero2:
-    numero1,numero2=intercambio_valores(numero1,numero2)
+        self.numero1=int(input("Ingresa el primer numero: "))
+        self.numero2=int(input("Ingresa el segundo numero: "))
+        self.numero3=int(input("Ingresa el tercer numero: "))
+
+    def ordenar_numeros(self):
+        if self.numero1>self.numero2:
+            self.numero1,self.numero2=self.intercambio_valores(self.numero1,self.numero2)
 
 
-if numero2>numero3:
-    numero2,numero3=intercambio_valores(numero2,numero3)
+        if self.numero2>self.numero3:
+            self.numero2,self.numero3=self.intercambio_valores(self.numero2,self.numero3)
 
-if numero1>numero2:
-    numero1,numero2=intercambio_valores(numero1,numero2)
+        if self.numero1>self.numero2:
+            self.numero1,self.numero2=self.intercambio_valores(self.numero1,self.numero2)
 
+    def imprimir_numeros(self):
+        print(f"Numeros ordenados: {self.numero1},{self.numero2},{self.numero3}")
+if __name__=="__main__":
+    numero1 =5
+    numero2=10
+    numero3=1
+    numeros = ordenadoTresNumeros(numero1,numero2,numero3)
+    print("\nNumeros desordenados\n")
+    numeros.imprimir_numeros()
+    print("\nNumeros ordenados\n")
+    numeros.ordenar_numeros()
+    numeros.imprimir_numeros()
 
-
-print(f"Numeros ordenados: {numero1},{numero2},{numero3}")
-print(f"El mayor es: {numero3}")
+    numeros.ingresar_numeros()
+    print("\nNumeros ordenados\n")
+    numeros.ordenar_numeros()
+    numeros.imprimir_numeros()
